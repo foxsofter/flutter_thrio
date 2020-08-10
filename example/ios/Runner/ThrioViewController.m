a052a8de64973e149e16e0ae619a5756a5f33264
foxsofter <foxsofter@gmail.com>
2020-08-10 20:33:08 +0800

    remove test code

--- example/ios/Runner/ThrioViewController.m
+++ example/ios/Runner/ThrioViewController.m
@@ -20,100 +20,65 @@
 @implementation ThrioViewController
 
 - (IBAction)pushFlutterPage:(id)sender {
-//  [ThrioNavigator pushUrl:@"/biz1/flutter1"];
-    
-    NSString* flutterAssetsName = [[NSBundle mainBundle] pathForResource:@"flutter_assets" ofType:@"zip"];
-    
-    NSString *unzipPath = [self tempUnzipPath];
-    
-    [SSZipArchive unzipFileAtPath:flutterAssetsName toDestination:unzipPath];
-    NSLog(@"flutter_assets path: %@", unzipPath);
-
-    NSString *bin = [NSString stringWithFormat:@"%@/flutter_assets/kernel_blob.bin", unzipPath];
-    NSLog(@"bin path: %@", bin);
-    if (![NSFileManager.defaultManager fileExistsAtPath:bin]) {
-        @throw @1;
-    }
-
-    FlutterDartProject *project = [[FlutterDartProject alloc] initWithFlutterAssetsURL: [NSURL URLWithString:[NSString stringWithFormat:@"%@/flutter_assets", unzipPath]] ];
-
-//    FlutterEngine *engine = [[FlutterEngine alloc] initWithName:@"hahaha" project:project allowHeadlessExecution:YES];
-    
-    FlutterViewController *vc = [[FlutterViewController alloc] initWithProject:project nibName:nil bundle:nil];
-    
-    [self.navigationController pushViewController:vc animated:YES];
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(500 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        self.navigationController.navigationBarHidden = YES;
-    });
-}
-
-- (NSString *)tempUnzipPath {
-    NSString *path = [NSString stringWithFormat:@"%@",
-                      NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0]
-                      ];
-    NSURL *url = [NSURL fileURLWithPath:path];
-    NSError *error = nil;
-    [[NSFileManager defaultManager] createDirectoryAtURL:url
-                             withIntermediateDirectories:YES
-                                              attributes:nil
-                                                   error:&error];
-    if (error) {
-        return nil;
-    }
-    return url.path;
+    [ThrioNavigator pushUrl:@"/biz1/flutter1"];
 }
 
 - (IBAction)popFlutter1:(id)sender {
-  [ThrioNavigator removeUrl:@"/biz1/flutter1"];
+    [ThrioNavigator removeUrl:@"/biz1/flutter1"];
 }
+
 - (IBAction)pushFlutter2:(id)sender {
-  [ThrioNavigator pushUrl:@"/biz2/flutter2"];
+    [ThrioNavigator pushUrl:@"/biz2/flutter2"];
 }
+
 - (IBAction)popFlutter2:(id)sender {
-  [ThrioNavigator removeUrl:@"/biz2/flutter2"];
+    [ThrioNavigator removeUrl:@"/biz2/flutter2"];
 }
 
 - (IBAction)pushNativePage:(id)sender {
-  [ThrioNavigator pushUrl:@"native1"];
+    [ThrioNavigator pushUrl:@"native1"];
 }
+
 - (IBAction)popNative1:(id)sender {
-  [ThrioNavigator removeUrl:@"native1"];
+    [ThrioNavigator removeUrl:@"native1"];
 }
+
 - (IBAction)pushNative2:(id)sender {
-  [ThrioNavigator pushUrl:@"native2" poppedResult:^(id _Nullable params) {
-    ThrioLogV(@"native2 popped: %@", params);
-  }];
+    [ThrioNavigator pushUrl:@"native2" poppedResult:^(id _Nullable params) {
+        ThrioLogV(@"native2 popped: %@", params);
+    }];
 }
+
 - (IBAction)popNative2:(id)sender {
-  [ThrioNavigator removeUrl:@"native2"];
+    [ThrioNavigator removeUrl:@"native2"];
 }
+
 - (IBAction)pop:(id)sender {
-  [ThrioNavigator pop];
+    [ThrioNavigator pop];
 }
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
+    [super viewDidLoad];
 
-  if (self.thrio_firstRoute.settings) {
-    NSString *txt = [NSString stringWithFormat:@"native page: %@ \n index: %@",
-                     self.thrio_firstRoute.settings.url,
-                     self.thrio_firstRoute.settings.index];
-    [self.label setText:txt];
-  } else {
-    // 只是给根部的ViewController标记url和index，这样才能定位到这个页面
-    [self thrio_pushUrl:@"native1"
-                  index:@1
-                 params:nil
-               animated:NO
-         fromEntrypoint:nil
-                 result:nil
-           poppedResult:nil];
-  }
+    if (self.thrio_firstRoute.settings) {
+        NSString *txt = [NSString stringWithFormat:@"native page: %@ \n index: %@",
+                         self.thrio_firstRoute.settings.url,
+                         self.thrio_firstRoute.settings.index];
+        [self.label setText:txt];
+    } else {
+        // 只是给根部的ViewController标记url和index，这样才能定位到这个页面
+        [self thrio_pushUrl:@"native1"
+                      index:@1
+                     params:nil
+                   animated:NO
+             fromEntrypoint:nil
+                     result:nil
+               poppedResult:nil];
+    }
 }
 
 - (void)onNotify:(NSString *)name params:(id)params {
-  ThrioLogV(@"native1 onNotify: %@, %@", name, params);
+    ThrioLogV(@"native1 onNotify: %@, %@", name, params);
 }
 
 @end
--- example/ios/Runner/ThrioViewController2.m
+++ example/ios/Runner/ThrioViewController2.m
@@ -11,76 +11,85 @@
 
 @interface ThrioViewController2 ()
 
-@property(weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
 @implementation ThrioViewController2
 
 - (IBAction)pushFlutterPage:(id)sender {
-  [ThrioNavigator pushUrl:@"/biz2/flutter2"
-             poppedResult:^(id _Nonnull params) {
-               ThrioLogV(@"/biz2/flutter2 popped: %@", params);
-             }];
+    [ThrioNavigator pushUrl:@"/biz2/flutter2"
+               poppedResult:^(id _Nonnull params) {
+        ThrioLogV(@"/biz2/flutter2 popped: %@", params);
+    }];
 }
-- (IBAction)popFlutter1:(id)sender {
-  [ThrioNavigator removeUrl:@"/biz2/flutter2"];
-}
-- (IBAction)pushNativePage:(id)sender {
-  [ThrioNavigator pushUrl:@"native1"];
-}
-- (IBAction)popNative1:(id)sender {
-  [ThrioNavigator removeUrl:@"native1"];
-}
-- (IBAction)popToNative1:(id)sender {
-  [ThrioNavigator popToUrl:@"native1"];
-}
-- (IBAction)pushNative1WithoutThrio:(id)sender {
-  UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-  UIViewController *vc =
-      [sb instantiateViewControllerWithIdentifier:@"ThrioViewController"];
 
-  [self.navigationController pushViewController:vc animated:YES];
+- (IBAction)popFlutter1:(id)sender {
+    [ThrioNavigator removeUrl:@"/biz2/flutter2"];
 }
+
+- (IBAction)pushNativePage:(id)sender {
+    [ThrioNavigator pushUrl:@"native1"];
+}
+
+- (IBAction)popNative1:(id)sender {
+    [ThrioNavigator removeUrl:@"native1"];
+}
+
+- (IBAction)popToNative1:(id)sender {
+    [ThrioNavigator popToUrl:@"native1"];
+}
+
+- (IBAction)pushNative1WithoutThrio:(id)sender {
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController *vc =
+        [sb instantiateViewControllerWithIdentifier:@"ThrioViewController"];
+
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
 - (IBAction)pop:(id)sender {
-  [ThrioNavigator popParams:@{@"k1" : @3}];
+    [ThrioNavigator popParams:@{ @"k1": @3 }];
 }
+
 - (IBAction)willPopYESNative2:(id)sender {
-  self.thrio_willPopBlock = ^(ThrioBoolCallback _Nonnull result) {
-    result(YES);
-  };
+    self.thrio_willPopBlock = ^(ThrioBoolCallback _Nonnull result) {
+        result(YES);
+    };
 }
+
 - (IBAction)willPopNONative2:(id)sender {
-  self.thrio_willPopBlock = ^(ThrioBoolCallback _Nonnull result) {
-    result(NO);
-  };
+    self.thrio_willPopBlock = ^(ThrioBoolCallback _Nonnull result) {
+        result(NO);
+    };
 }
+
 - (IBAction)willPopNilNative2:(id)sender {
-  self.thrio_willPopBlock = nil;
+    self.thrio_willPopBlock = nil;
 }
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
+    [super viewDidLoad];
 
-  if (self.thrio_firstRoute.settings) {
-    NSString *txt =
-        [NSString stringWithFormat:@"native page: %@ \n index: %@",
-                                   self.thrio_firstRoute.settings.url,
-                                   self.thrio_firstRoute.settings.index];
-    [self.label setText:txt];
-  }
+    if (self.thrio_firstRoute.settings) {
+        NSString *txt =
+            [NSString stringWithFormat:@"native page: %@ \n index: %@",
+             self.thrio_firstRoute.settings.url,
+             self.thrio_firstRoute.settings.index];
+        [self.label setText:txt];
+    }
 
-  self.thrio_hidesNavigationBar = YES;
+    self.thrio_hidesNavigationBar = YES;
 
-  // 禁用手势，可以点击返回键关闭页面
-  //  self.thrio_willPopBlock = ^(ThrioBoolCallback _Nonnull result) {
-  //    result(YES);
-  //  };
+    // 禁用手势，可以点击返回键关闭页面
+    //  self.thrio_willPopBlock = ^(ThrioBoolCallback _Nonnull result) {
+    //    result(YES);
+    //  };
 
-  // 禁用手势，也不能点击返回键关闭页面
-  //  self.thrio_willPopBlock = ^(ThrioBoolCallback _Nonnull result) {
-  //    result(NO);
-  //  };
+    // 禁用手势，也不能点击返回键关闭页面
+    //  self.thrio_willPopBlock = ^(ThrioBoolCallback _Nonnull result) {
+    //    result(NO);
+    //  };
 }
 
 @end
